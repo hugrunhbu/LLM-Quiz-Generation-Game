1490a3bd20e1c8d3678f672d3bf83c0f520531a4
Hugrun Helgadottir <hugrunh6@bu.edu>
2025-04-29 13:34:48 -0400

    adding the UI using React

---
+++ ai-quiz-frontend/src/components/GeneratingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GeneratingScreen = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Generating your quiz...</h2>
+      <p>Should take less than 10 seconds don't worry!</p>
+    </div>
+  );
+};
+
+export default GeneratingScreen;
