b47533660812aa7fb4487b3ac4b4db7236c7fc32
Hugrun Helgadottir <hugrunh6@bu.edu>
2025-04-29 17:11:23 -0400

    some UI updates

--- ai-quiz-frontend/src/components/GeneratingScreen.js
+++ ai-quiz-frontend/src/components/GeneratingScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./GeneratingScreen.css";
 
 const GeneratingScreen = () => {
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>Generating your quiz...</h2>
-      <p>Should take less than 10 seconds don't worry!</p>
+    <div className="generating-container">
+      <div className="spinner"></div>
+      <h2 className="generating-text">Generating your quiz...</h2>
+      <p className="generating-text">
+        This should take less than 10 seconds don't worry!
+      </p>
     </div>
   );
 };
